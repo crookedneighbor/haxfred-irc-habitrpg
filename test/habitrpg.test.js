f50023b5047e969d1e1ec972664fa5f2b72eff2d
Blade Barringer <blade@crookedneighbor.com>
2014-09-24 21:03:46 -0500

    Started writing helpful tests

--- test/habitrpg.test.js
+++ test/habitrpg.test.js
@@ -6,8 +6,8 @@ var path = require('path');
 var chai = require('chai');
 
 var Haxfred = require('haxfred');
-var irc_habit = require('../lib/haxfred-irc-habitrpg');
 var helpers = require('../lib/helpers');
+var irc_habit = require('../lib/haxfred-irc-habitrpg');
 
 chai.use(expect);
 chai.use(assert);
@@ -28,74 +28,68 @@ describe('Sending Data to HabitRPG', function () {
       channels: [
         '#foo'
       ],
-      habitRPGUsers: { bob: ["00000", "AAAAAA"] },
+      habitRPGUsers: '../test/testconfig.json',
       rootDir: path.resolve(__dirname, '../lib')
     });
 
     haxfred.initialize();
   });
 
-   describe('getHabitCredentials()', function () {
-      
-      var users = {alice: ["00000", "AAAAA"], bob: ["11111", "BBBBB"], charlie: ["22222", "BBBBB"]};
+   describe('listen for upvote event and if username is in config file, call sendToHabit()', function () {
 
-      it('expect to get bob\'s credentials when passing his exact name', function () {
-        var username = "bob";
-        expect(helpers.getHabitCredentials(users, username)).to.deep.equal(users['bob']);
-      });
-      it('expect to get bob\'s credentials when passing his name with a captial letter', function () {
-        var username = "Bob";
-        expect(helpers.getHabitCredentials(users, username)).to.deep.equal(users['bob']);
-      });
-      it('expect to get bob\'s credentials when passing his name with a mixed casing', function () {
-        var username = "bOB";
-        expect(helpers.getHabitCredentials(users, username)).to.deep.equal(users['bob']);
-      });
-      it('expect to not get credentials when passing a user that is not in the list', function () {
-        var username = "foo";
-        expect(helpers.getHabitCredentials(users, username)).to.not.be.ok;
-      });
-   });
-   /*
-    * @TODO: Ask for help in writing this test
-    *
-   describe('sendToHabit()', function () {
-      
-      var users = {alice: ["00000", "AAAAA"], bob: ["11111", "BBBBB"], charlie: ["22222", "BBBBB"]};
+     beforeEach(function() {
+       sinon.stub(helpers, "sendToHabit");
+     });
 
-      it('expect to get an error when sending faulty habit creds', function () {
-        var id = users['bob'][0];
-        var api = users['bob'][1];
-        var habit = "foo";
-        var direction = true;
+     afterEach(function() {
+       helpers.sendToHabit.restore();
+     });
 
-        expect(helpers.sendToHabit(id, api, habit, direction)).to.throw(Error);
-      });
-   });
-   */
-
-   /*
-    * @TODO: Ask for help in writing this test
-    *
-   describe('emit upvote event', function () {
-     // Since we dont really want to setup the irc module for a test
-
-     it('should emit an upvote event', function(done){
-          var habitSpy = sinon.spy();
-          setTimeout(function(){
-            assert(habitSpy.called, 'Upvote event did not fire.');
-            assert(habitSpy.calledOnce, 'Upvote fired more than once');
-            done();
-          }, 1000);
-          haxfred.on('irc.habitrpg', habitSpy);
+     it('username is in config file, should call sendToHabit()', function(done){
           
-          haxfred.emit('irc.upvote', {
-             recipient: 'alice',
-             sender: 'bob',
-          });
+        haxfred.emit('irc.upvote', {
+           recipient: 'Alice',
+           sender: 'bob',
+           onComplete: function() {
+             sinon.assert.calledWithMatch(helpers.sendToHabit, "ping", "pong", "haxfred", true);
+             done();
+           }
+        });
 
-       done();
+
+     });
+
+     it('username is not in config file, should not call sendToHabit()', function(done){
+          
+        haxfred.emit('irc.upvote', {
+           recipient: 'JERK',
+           sender: 'bob',
+           onComplete: function() {
+             sinon.assert.neverCalledWith(helpers.sendToHabit, "ping", "pong", "haxfred", true);
+             done();
+           }
+        });
+
+
      });
    });
-   */
+
+   /*
+   describe('sendToHabit()', function() {
+     beforeEach(function() {
+       sinon.stub(habitapi, "updateTaskScore");
+     });
+
+     afterEach(function() {
+       habitUser.updateTaskScore.restore();
+     });
+
+     it('calls habitapi.user.updateTaskScore()', function(done) {
+       helpers.sendToHabit('alice', 'apiKeyMan', 'robotcop', true);
+       sinon.assert.calledWithMatch('robotcop', true);
+     });
+     it('calls habitapi.user.updateTaskScore with default direction');
+     it('calls habitapi.user.updateTaskScore with default id');
+   });*/
+
 });
