7708e9401e3ea9c8d895c103b5717ab7fcdcf2e8
Blade Barringer <blade@crookedneighbor.com>
2014-09-29 16:26:45 -0500

    Added tests for emits helper

--- test/habitrpg.test.js
+++ test/habitrpg.test.js
@@ -189,8 +189,83 @@ describe('Testing user helper', function() {
 });
 
 describe('Testing emits helper', function() {
-  it('Expect ', function() {
+  var default_id = "not haxfred",
+      config = {
+        "irc.foo": {},
+        "irc.bar": {
+          id: "bar",
+          recipient: "from",
+          direction: false
+        }, 
+        "irc.ping": {
+          direction: "down"
+        },
+        "irc.pong": {
+          direction: true
+        },
+        "irc.zing": {
+          direction: "up"
+        },
+        "irc.zow": {
+          direction: "zing"
+        }
+      },
+      emits = emitHelper.formatEmits(config);
 
+  it('Expect emit with no id specified to use the default id', function() {
+    expect(emits["irc.foo"]).to.have.property('id').to.equal("haxfred");
+  });
 
+  it('Expect emit with no recipient specified to use the default recipient', function() {
+    expect(emits["irc.foo"]).to.have.property('recipient').to.equal("recipient");
+  });
+
+  it('Expect emit with no direction specified to use the default direction', function() {
+    expect(emits["irc.foo"]).to.have.property('direction').to.equal(true);
+  });
+
+  it('Expect emit with specified id to have the specified id', function() {
+    expect(emits["irc.bar"]).to.have.property('id').to.equal("bar");
+  });
+
+  it('Expect emit with specified recipient to have the specified recipient', function() {
+    expect(emits["irc.bar"]).to.have.property('recipient').to.equal("from");
+  });
+
+  it('Expect emit with specified direction to have the specified direction', function() {
+    expect(emits["irc.bar"]).to.have.property('direction').to.equal(false);
+  });
+
+  it('Expect emit with specified direction of "down" to have a direction of false', function() {
+    expect(emits["irc.ping"]).to.have.property('direction').to.equal(false);
+  });
+
+  it('Expect emit with specified direction of "up" to have a direction of true', function() {
+    expect(emits["irc.pong"]).to.have.property('direction').to.equal(true);
+  });
+
+  it('Expect emit with specified direction of anything other than down or false to have a direction of true', function() {
+    expect(emits["irc.zow"]).to.have.property('direction').to.equal(true);
+  });
+
+  it('Expect emits object to return false when a string is passed in instead of an object', function() {
+    var emitString = emitHelper.formatEmits("string");
+    expect(emitString).to.equal(false);
+  });
+
+  it('Expect emits object to return false when a number is passed in instead of an object', function() {
+    var emitNumber = emitHelper.formatEmits(42);
+    expect(emitNumber).to.equal(false);
+  });
+
+  it('Expect emits object to return false when blank object is passed in', function() {
+    var blankEmit = emitHelper.formatEmits({});
+    expect(blankEmit).to.equal(false);
+  });
+
+  it('Expect emit with no id specified to use default id passed into function', function() {
+    var configObj = {"irc.foo": {}};
+    var emitsObj = emitHelper.formatEmits(configObj, default_id);
+    expect(emitsObj["irc.foo"]).to.have.property('id').to.equal("not haxfred");
   });
 });
